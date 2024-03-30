1145475dd912f29702ddd3c275e313386864a1fb
GERALD ADDEY <geraldaddey@yahoo.com>
2024-03-30 20:47:35 +0000

    feat: clean_up and predict classes

--- training.py
+++ training.py
@@ -3,15 +3,16 @@ import json
 import pickle
 import numpy as np
 import nltk 
+import pandas as pd
+import tensorflow  as tf
 from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.optimizers import SGD
 
-# Load the intents from the JSON file
 intents = json.loads(open('intents.json').read())
 
-# Initialize the WordNet lemmatizer
+
 lemmatizer = WordNetLemmatizer()
 
 # Initialize empty lists for words, classes, and documents
@@ -22,28 +23,68 @@ documents = []
 # Define a list of characters to ignore
 ignore_letters = ['?', '!', '.', ',']
 
-# Iterate through each intent in the intents JSON
 for intent in intents['intents']:
-    # Iterate through each pattern in the intent
     for pattern in intent['patterns']:
-        # Tokenize the pattern into a list of words
+
         word_list = nltk.word_tokenize(pattern)
-        # Extend the words list with the word_list
+
         words.extend(word_list)
-        # Append the word_list and intent tag as a tuple to the documents list
+
         documents.append((word_list, intent['tag']))
         
-        # If the intent tag is not already in the classes list, add it
+
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-# Lemmatize the words and remove the ignore_letters
+
 words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
-# Sort and remove duplicates from the words list
+
 words = sorted(set(words)) 
+print(words)
 
 pickle.dump(words, open('words.pkl', 'wb'))
 pickle.dump(classes, open('classes.pkl', 'wb'))
 
-# Print the words list
-print(words)
+
+
+training = []
+output_empty = [0] * len(classes)
+
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([np.array(bag), np.array(output_row)])  # Convert bag and output_row to numpy arrays
+    
+# random.shuffle(training)
+max_length = max(len(bag) for bag, _ in training)
+
+# Pad all 'bag' arrays to the maximum length with zeros
+for i in range(len(training)):
+    bag, output_row = training[i]
+    if len(bag) < max_length:
+        padding = np.zeros(max_length - len(bag))
+        training[i] = (np.concatenate((bag, padding)), output_row)
+
+# Now, you can convert 'training' to a NumPy array without errors
+training = np.array(training)
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
+print(f"training: {training} \n train_x: {train_x} \n train_y: {train_y}" )
+
+model  = Sequential() 
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu')) 
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', model)
+print('Model created')
